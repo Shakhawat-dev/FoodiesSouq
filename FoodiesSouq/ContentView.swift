958f7128ead8eafe99b93f1ba00475699229b1c1
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2021-10-16 21:24:22 +0600

    Initial Commit

---
+++ FoodiesSouq/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodiesSouq
+//
+//  Created by Shakhawat Hossain Shahin on 10/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodiesSouq/FoodiesSouqApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodiesSouqApp.swift
+//  FoodiesSouq
+//
+//  Created by Shakhawat Hossain Shahin on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FoodiesSouqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
